407ea06f009670c47b76aefffdf7791c4335baed
Abhishek Auti <104745389+Abhi31452@users.noreply.github.com>
2024-02-13 15:11:14 +0000

    commit

---
+++ 02_basic/01_array.js
@@ -0,0 +1,46 @@
+// array ....non homogeneous....create shallow copy 
+           //shallow copy......gives reference to a copy
+           //deep copy ......not give ref to copy 
+
+// const myArr = [0, 1, 2, 3, "Abhi", "jb"]
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+// slice :myArr.slice(1, 3).... doesnot include last    element & also doesn't  manipulate org array
+// splice :myArr.splice(1, 3)....  include last     elementof range & also manipulate org array
